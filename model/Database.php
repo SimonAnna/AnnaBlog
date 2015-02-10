a31d14c9e9ce78a84615c13866f51cac63752119
Anna Simon <anna@rocknrollrobots25.com>
2015-02-09 18:36:27 -0800

    on vid 28

--- model/Database.php
+++ model/Database.php
@@ -1,8 +1,16 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Database {
+    private $connection;
+    private $host;
+    private $username;
+    private $password;
+    private $database;
+    
+    public function __construct($host, $username, $password, $database){
+      $this->host = $host;
+      $this->username = $username;
+      $this->password = $password;
+      $this->database = $database;  
+    }
+}
 
